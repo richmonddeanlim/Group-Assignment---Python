b925adb01f55535492bdf449ab07c050c6c7c42a
Richmond Deanlim <richmonddeanlim14@gmail.com>
2024-12-02 14:33:36 +0800

    adding last function to student.py

--- function/student.py
+++ function/student.py
@@ -40,6 +40,7 @@ path_grade = "database/student_grade.txt"
 # Data Function 
 # Reading data and assign it to list and make it usable
 def data_reading(path):
+    try:
         # Making the txt file to variable
         with open(path, "r") as file:
             raw_data = file.readlines()
@@ -56,6 +57,9 @@ def data_reading(path):
             record_data.append(usable_data)
 
         return record_data
+    
+    except FileNotFoundError:
+        print("pls open with the main file directory location")
 
 # View Avaible Module
 # the value return the avible module and student id index(0,1)
@@ -87,7 +91,7 @@ def view_module():
         # Cheking student have enrool or not [preventing duplicate data]
         student_module = []
         for student in student_record:
-            if student_id.lower() == student[0]:
+            if student_id == student[0]:
                 for i in range (3,len(student)):
                     if len(student) == 4:
                         student_module.append(student[3])
@@ -114,9 +118,7 @@ def view_module():
 
         return avaible_module,student_id
     
-    except KeyboardInterrupt or FileNotFoundError:
-        if FileNotFoundError:
-            print('pls open from the main file directory')
+    except KeyboardInterrupt:
         pass
 
 # Enroll in module
@@ -156,9 +158,10 @@ def enroll():
         
         print("\nData have been updated\n")
 
-    except KeyboardInterrupt or FileNotFoundError:
-        if FileNotFoundError:
-            print('pls open from the main file directory')
+    except FileNotFoundError:
+        print('pls open from the main file directory')
+    
+    except KeyboardInterrupt :
         pass
 
 # View Grades
@@ -205,11 +208,129 @@ def view_grade():
         else:
             print("No Module are avaible")
 
-    except KeyboardInterrupt or FileNotFoundError:
-        if FileNotFoundError:
-            print('pls open from the main file directory')
-        pass
+    except KeyboardInterrupt:
+       pass
 
 #View Student attendance
-                
-view_grade()
+def student_attendance():
+    try:
+        # Getting the data
+            student_record = data_reading(path_student)
+            attendance_record = data_reading(path_attendance)
+
+            # #Getting User Tp number
+            while True:
+                student_id = input("Input student id (TPxxxxxx): ").strip().upper()
+                # Validating input
+                for student in student_record:
+                    if (("TP" or "Tp" or "tp" in student_id) and len(student_id) == 8) and student_id == student[0]:
+                        condition = True
+                        break
+                    else:
+                        condition = False
+                if condition == True:
+                    break
+                elif condition == False:      
+                    print("Tp is not found")
+
+            # Fetch the student module name and grade based on student id
+            attendance_list = []
+            for data in attendance_record:
+                if student_id.upper() == data[0]:
+                    attendance_list.append(float(data[2]))
+                    attendance_list.append(float(data[3]))
+
+            percentage = (attendance_list[0]/(attendance_list[1]+attendance_list[0]))*100
+
+            print(f"Your attendance percentage is: {percentage:.2f}%")
+
+    except ZeroDivisionError:
+        if attendance_list[0] == 0 and attendance_list[1] == 0:
+            print("You never attend or absent any classes")
+        elif attendance_list[0] > 0 and attendance_list[1] == 0:
+            print("Zero Devision Error")
+    
+    except KeyboardInterrupt:
+        pass
+
+#Unenroll Function
+def unenroll():
+    try:
+        # Getting data that returned by the function
+        student_record = data_reading(path_student)
+        
+        #Getting User Tp number
+        while True:
+            student_id = input("Input student id (TPxxxxxx): ").strip().upper()
+            # Validating input
+            for student in student_record:
+                if (("TP" or "Tp" or "tp" in student_id) and len(student_id) == 8) and student_id == student[0]:
+                    condition = True
+                    break
+                else:
+                    condition = False
+            if condition == True:
+                break
+            elif condition == False:      
+                print("Tp is not found")
+
+        # Cheking student have enrool or not [preventing duplicate data]
+        unenroll_module = []
+        for student in student_record:
+            if student_id == student[0]:
+                for i in range (3,len(student)):
+                    if len(student) == 4:
+                        unenroll_module.append(student[3])
+                    else: 
+                        unenroll_module.append(student[i])
+
+        if unenroll_module:
+            # Print header
+            print("=" * 30)
+            print(f"{"No":^4} {'Module ID':^12} ")
+            print("=" * 30)
+            # Print each data with format string ( using ^ to make center alignment)
+            num = 0
+            for module_id in unenroll_module:
+                num += 1
+                print(f"{num:^4} {module_id:^12} ")
+            print("=" * 30)
+
+        else:
+            print("No Module are avaible")
+    
+        # Getting user input and getting validation
+        while True:
+            unenroll_choice = input("Enter the module id to unenroll: ")
+            if unenroll_choice.upper() in [item for item in unenroll_module]:
+                break
+            else:
+                print("You input the wrong id")
+        
+        # Read the txt file data
+        with open(path_student,"r") as file:
+            data = file.readlines()
+        
+        # finding the index of tp number
+        i = 0
+        for item in student_id:
+            if item in student_id:
+                break
+            else:
+                i += 1
+
+        # Replacing the line in certain index
+        data[i] = data[i].replace(str(f",{unenroll_choice}"), str(f""))
+
+        # Rewrite the updated file
+        with open(path_student,"w") as file:
+            file.writelines(data)
+        
+        print("\nYou have sucessfully unenroll from the module")
+
+    except FileNotFoundError:
+        print('pls open from the main file directory')
+    
+    except KeyboardInterrupt :
+        pass
+
